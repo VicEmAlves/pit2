c564b1e330363e75563f3d7195deacada2f544ee
emanu <emanu@DESKTOP-L3QRRV2>
2025-11-14 21:28:28 -0300

    Add connection string

--- api/src/functions/testTrigger.js
+++ api/src/functions/testTrigger.js
@@ -1,9 +1,13 @@
 const { app } = require('@azure/functions');
 
+const connectionString = process.env.DATABASE_CONNECTION_STRING;
+
+
 app.http('testTrigger', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        return { body: JSON.stringify({ "text": `Hello, from the API!` }) };
+
+        return { body: JSON.stringify({ "text": `Hello, from the API! ${connectionString}` }) };
     }
 });
